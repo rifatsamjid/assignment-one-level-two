99c19d010790724d5e2170a41489dd18af07050f
RI F AT <rifattamim74328@gmail.com>
2026-05-07 16:12:06 +0600

    solve the problem three

--- solutions.ts
+++ solutions.ts
@@ -11,3 +11,18 @@ function reverseString(text:string):string {
     return text.split("").reverse().join('');
 }
 
+
+
+
+// problem 3
+
+type StringOrNumber = string | number;
+
+function checkType(value : StringOrNumber):string {
+    if(typeof value === 'string'){
+        return "String"
+    }else{
+        return "number"
+    }
+}
+
